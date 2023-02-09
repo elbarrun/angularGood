abdd8343c1f6e5c262805fb3d437888c5d4d841e
iker <dam10.2022.jesuitas@gmail.com>
2023-02-09 18:40:18 +0100

    ""

--- src/app/libroclicked.service.ts
+++ src/app/libroclicked.service.ts
@@ -4,6 +4,20 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class LibroclickedService {
+  libros: Array<object>
 
-  constructor() { }
+  constructor() { 
+    this.libros=[];
+  }
+
+  libroVisto(libroVisto:any){
+    this.libros.push(libroVisto);
+  }
+  verListado(){
+    if(this.libros.length > 0){
+      return this.libros
+    }else{
+      return false;
+    }
+  }
 }
